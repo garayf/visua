24af83045b7974767dc5c34ffccacaf59e8c51ff
diarodriguezmo <diarodriguezmo@unal.edu.co>
2020-09-28 16:45:40 -0500

    Update munker.js

--- _illusions/munker.js
+++ _illusions/munker.js
@@ -1,5 +1,6 @@
 function setup() {
-  createCanvas(800, 400);
+  var canvas = createCanvas(400, 400); // Keep canvas reference
+  canvas.parent('sketch-div'); // Necessary to position canvas
   noStroke();
 }
 
@@ -8,20 +9,22 @@ function draw() {
   
   let a = color(0, 126, 255, 255);
   let b = color(255, 225, 0, 255)
-  let c = color(255, 0, 00, 255)
+  let c = color(255, 0, 0, 255)
   fill(a);
-  rect(0, 0, 400, 400); // Sets 'value' to 102
+  rect(0, 0, 400, 200); // Sets 'value' to 102
   fill(c);
-  circle(200, 200, 350);
+  circle(100, 100, 150);
+  circle(300, 100, 150);
   fill(b);
-  rect(400, 0, 400, 400);
-    for (let i = 0; i < width/2; i+=12) {
-    rect(i, 0, 6, 400);
+  rect(0, 200, 400, 200);
+    for (let i = 0; i < width; i+=12) {
+    rect(i, 0, 6, 200);
   }
   fill(c);
-  circle(600, 200, 350);
+  circle(100, 300, 150);
+  circle(300, 300, 150);
   fill(a);
-  for (let i = width/2+2; i < width; i+=12) {
-    rect(i, 0, 6, 400);
+  for (let i = 6; i < width; i+=12) {
+    rect(i, 200, 6, 200);
   }
 }
