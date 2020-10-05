465bba4e0aabac5ae0987422dd8de71a10bd4e8e
edgaray <32691733+garayf@users.noreply.github.com>
2020-10-05 00:18:36 -0500

    Update template.js

--- _sketches/template.js
+++ _sketches/template.js
@@ -1,11 +1,28 @@
-// p5 sketch template
-function setup() {
-  var canvas = createCanvas(400, 400); // Keep canvas reference
-  canvas.parent('sketch-div'); // Necessary to position canvas
+function preload() {
+  img = loadImage("data/download.jpg");
 }
 
-function draw() {
-  background(255);
-  ellipse(mouseX,mouseY,80,80);
+function setup() {
+    var canvas = createCanvas(400, 400);
+    canvas.parent('sketch-div');
+
+}
+
+function draw(){   
+    
+    img.loadPixels();
+    /*
+    for (let i = 0; i < pixels.length; i+=4) {
+        let c = color(pixels[i], pixels[i + 1], pixels[i + 2], pixels[i + 3])
+        if (brightness(c) < tresshold) {
+            pixels[i] = pixels[i + 1] = pixels[i + 2] = 0;
+        } else {
+            pixels[i] = pixels[i + 1] = pixels[i + 2] = 255;
+        }
+     }
+     */
+    updatePixels();
+   
+    image(img,0,0)
 }
 
