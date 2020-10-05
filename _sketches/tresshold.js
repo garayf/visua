8357475e38db18ad08bf492dd6a608039def0e43
Maria Alejandra Robayo Sepúlveda <marobayos@unal.edu.co>
2020-10-04 23:07:04 -0500

    :bug: hotfix

--- _sketches/tresshold.js
+++ _sketches/tresshold.js
@@ -7,7 +7,6 @@ function preload() {
 function setup() {
     var canvas = createCanvas(400, 400);
     canvas.parent('sketch-div');
-    strokeCap(PROJECT);
 }
 
 function draw(){
@@ -19,6 +18,6 @@ function draw(){
         } else {
             pixels[i] = pixels[i + 1] = pixels[i + 2] = pixels[i + 3] = 255;
         }
-        updatePixels();
     }
+    updatePixels();
 }
